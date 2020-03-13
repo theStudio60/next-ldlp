4a2f91347a7266b1ba196779ac75c40e995ada29
Fabien Dupont <fab.d1987@yandex.com>
2020-03-13 18:13:17 +0100

    add fixed-top className

--- components/header/Navbar.js
+++ components/header/Navbar.js
@@ -2,7 +2,7 @@ import Navheader from "./Navheader" ;
 import Navlist from "./Navlist" ;
 
 const Navbar = () => (
-  <nav className="navbar navbar-expand-lg bg-dark">
+  <nav className="navbar navbar-expand-lg bg-dark  fixed-top">
     <div className="container-fluid">
       <Navheader/>
       <Navlist/>
