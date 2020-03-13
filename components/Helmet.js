1c52ae3e2788df59faebc0e083e22dcb49013a75
Fabien Dupont <fab.d1987@yandex.com>
2020-03-13 21:33:23 +0100

    insert meta tags

--- components/Helmet.js
+++ components/Helmet.js
@@ -1,6 +1,23 @@
 //SEO
+import { attributes, html } from '../config/seo.md';
 
-/*
-const Helmet = () => ();
+const Helmet = () => (<>
+    <meta charSet="utf-8" />
+    
+    <title>{attributes.Title}</title>
+    <noscript><meta httpEquiv="refresh"/></noscript>
+
+    <meta property="og:site_name" content={attributes.Title}/>
+    <meta property="og:url" content={attributes.Url} />
+    <meta property="og:image" content=" " />
+    <meta property="og:locale" content="en_GB" />
+    
+    
+    <link rel="canonical" href= {attributes.Url} />
+    <meta name="referrer" content="origin-when-crossorigin" id="meta_referrer" />
+
+    <meta name="description" content={attributes.MetaDescription} />
+
+
+</>);
 export default Helmet;
-*/
