0ca9980fcd7bb6a5775222a60a45ac2aaae34940
Fabien Dupont <fab.d1987@yandex.com>
2020-03-13 17:24:14 +0100

    Create Layout.js

---
+++ components/Layout.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Layout = () => (
+  <div>
+    <Header/>
+        
+    <Footer/>
+  </div>
+);
+export default Layout;
