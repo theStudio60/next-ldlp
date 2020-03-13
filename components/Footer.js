90fb07d6cb9e435614e4d0b0c027dd6e6ae2d158
Fabien Dupont <fab.d1987@yandex.com>
2020-03-13 17:23:45 +0100

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+
+const Footer = () => (
+  <footer className="footer pt-4 mt-4  text-center text-md-left">
+    <div className="container-fluid">
+        © 2020 - <a href="https://github.com/fabien-dupont"> Fabien Dupont</a> 
+     </div>
+  </footer>
+);
+
+export default Footer;
