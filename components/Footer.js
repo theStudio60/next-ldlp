383dc96c7e1b9e55a312264a8cefb5af583e622f
Fabien Dupont <fab.d1987@yandex.com>
2020-03-13 18:13:00 +0100

    removed author

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@
 const Footer = () => (
   <footer className="footer pt-4 mt-4  text-center text-md-left">
     <div className="container-fluid">
-        © 2020 - <a href="https://github.com/fabien-dupont"> Fabien Dupont</a> 
+        © 2020 
      </div>
   </footer>
 );
