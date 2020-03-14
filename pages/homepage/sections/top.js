f1647f6a3430f7b68fd9d77fee951f3dbdb148ad
Fabien Dupont <fab.d1987@yandex.com>
2020-03-14 10:45:41 +0100

    Update top.js

--- pages/homepage/sections/top.js
+++ pages/homepage/sections/top.js
@@ -1,9 +1,9 @@
 import Section from '../../../components/Section';
-
+//THis is the TOP section of the homepage
 const topSectionBody = {minHeight:"100vh",backgroundColor:"#222",color:"#eee",paddingTop:"25vh",paddingBottom:"25vh"};
 
 const Homepage_Section_Top = (props) => (<>
-<Section className="section m-0 p-0" >
+<Section className="section m-0 p-0" id="s_00" >
   <div className="container-fluid m-0" style={topSectionBody}>
     <div className="row" >
       {props.children}
