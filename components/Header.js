a21dbc4c392fc21b6c09399cab3f7cd30ef3adfb
Fabien Dupont <fab.d1987@yandex.com>
2020-03-14 10:44:28 +0100

    add main.scss

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,9 @@ import Helmet from './Helmet';
 const Header = () => (<>
   <Head>
     <Helmet/>
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"/>
+    <link rel="stylesheet" href="../src/main.scss" />
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"/> 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"/>
   </Head>
