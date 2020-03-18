93f759171ba0d8c0f92a40ecdf69bd98ad1def50
Fabien Dupont <fab.d1987@yandex.com>
2020-03-18 14:46:59 +0100

    remove navlist

--- components/header/Navbar.js
+++ components/header/Navbar.js
@@ -1,11 +1,9 @@
 import Navheader from "./Navheader" ;
-import Navlist from "./Navlist" ;
 
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg bg-dark  fixed-top">
     <div className="container-fluid">
       <Navheader/>
-      <Navlist/>
     </div>
   </nav>
 );
